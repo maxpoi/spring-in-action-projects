88005f0578baa0ba0f0b488fec18a6062e0a84b8
maxpoi <misaki.cy@gmail.com>
2021-02-07 23:05:26 +1100

    to be continued.

--- src/main/java/springinaction/tacocloudexample/controllers/DesignController.java
+++ src/main/java/springinaction/tacocloudexample/controllers/DesignController.java
@@ -3,13 +3,17 @@ package springinaction.tacocloudexample.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springinaction.tacocloudexample.objects.Ingredient;
 import springinaction.tacocloudexample.objects.Ingredient.Type;
 import springinaction.tacocloudexample.objects.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,5 +55,16 @@ public class DesignController {
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 }

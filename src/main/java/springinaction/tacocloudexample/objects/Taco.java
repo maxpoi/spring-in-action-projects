a82d5d5e9902fd767557656bde20ad37127cf63c
maxpoi <misaki.cy@gmail.com>
2021-02-07 14:45:48 +1100

    Initial commit

---
+++ src/main/java/springinaction/tacocloudexample/objects/Taco.java
@@ -0,0 +1,12 @@
+package springinaction.tacocloudexample.objects;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+}
---
+++ src/test/java/springinaction/tacocloudexample/TacoCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springinaction.tacocloudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
